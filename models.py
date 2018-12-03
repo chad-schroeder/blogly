2e3050ded682e95609e5cf7a80932884011f8e86
Chad Schroeder <gmchad@gmail.com>
2018-12-03 15:53:38 -0800

    PostTag model, Tag model, HTML

--- models.py
+++ models.py
@@ -41,7 +41,40 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
 
+    post_tags = db.relationship('PostTag', backref="post")
+
     def __repr__(self):
         """Show info for user."""
 
         return f"<Post id={self.id}, Post title={self.title}, content={self.content}, created at={self.created_at} user_id={self.user_id}>"
+
+
+class Tag(db.Model):
+    """Posts tags."""
+
+    __tablename__ = "tags"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.Text, nullable=False, unique=True)
+
+    post_tags = db.relationship("PostTag", backref="tag")
+
+    def __repr__(self):
+        """Show tag."""
+
+        return f"<Tag id={self.id}, name={self.name}>"
+
+
+class PostTag(db.Model):
+    """Posts tags (joins a Post and a Tag)."""
+
+    __tablename__ = "post_tags"
+
+    post_id = db.Column(
+        db.Integer, db.ForeignKey('posts.id'), primary_key=True)
+    tag_id = db.Column(db.Integer, db.ForeignKey('tags.id'), primary_key=True)
+
+    def __repr__(self):
+        """Show PostTag."""
+
+        return f"<PostTag post_id={self.post_id}, tag_id={self.tag_id}>"
